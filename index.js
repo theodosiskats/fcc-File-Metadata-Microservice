10696952fca2c88493c52ba66420da06bcef94e8
TheodosisKats <theodosis.kats@gmail.com>
2023-02-09 00:03:30 +0200

    fcc-File Metadata Microservice

---
+++ index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var cors = require('cors');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' })
+require('dotenv').config()
+
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse', upload.single('upfile', (err, res) => {
+  if (err) return console.log(err.message);
+  return console.log("done")
+}), (req, res) => {
+  const { originalname: name, mimetype: type, size } = req.file;
+  res.json({
+    name,
+    type,
+    size
+  })
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
+});
